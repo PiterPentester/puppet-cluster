a0654b282b08b0d957a7b9e198f7c3763033795a
Bohdan Pakhomov <shtormless@gmail.com>
2020-05-12 17:36:11 +0300

    cluster: create 1 master & 2 agents

---
+++ bootstrap-agent.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# Bootstraping puppet AGENT
+
+# add repo
+rpm -Uvh https://yum.puppet.com/puppet6-release-el-7.noarch.rpm
+
+# update
+yum -y update
+
+# install ntp
+yum -y install ntp ntpdate
+
+# configure ntp
+ntpdate 0.centos.pool.ntp.org
+
+# enable ntpdate
+systemctl enable ntpd
+
+# disable selinux
+sed -i 's/SELINUX=enabled/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+# install Java 1.8
+yum install -y java-1.8.0-openjdk wget
+
+# install AGENTS
+yum install -y puppet-agent
+
+# configure AGENT
+echo "[main]
+ certname = ${hostname_w}
+ server = master.puppet.io
+ environment = production
+ runinterval = 10m" >> /etc/puppetlabs/puppet/puppet.conf
+
+# configure hostnames
+
+if [ "$(hostname)" == "worker1" ]; then
+  echo "10.0.0.10    master.puppet.io puppet
+  10.0.0.12    worker2" >> /etc/hosts
+elif [ "$(hostname)" == "worker2" ]; then
+  echo "10.0.0.10    master.puppet.io puppet
+  10.0.0.11    worker1" >> /etc/hosts
+fi
+
+# run AGENT
+/opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
---
+++ bootstrap-master.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Bootstraping puppet MASTER
+
+# configure hosts
+echo "10.0.0.11    worker1
+10.0.0.12    worker2" >> /etc/hosts
+
+# add repo
+rpm -Uvh https://yum.puppet.com/puppet6-release-el-7.noarch.rpm
+
+# update
+yum -y update
+
+# install ntp
+yum -y install ntp ntpdate
+
+# configure ntp
+ntpdate 0.centos.pool.ntp.org
+
+# enable ntpdate
+systemctl enable ntpd
+
+# disable selinux
+sed -i 's/SELINUX=enabled/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+# install Java 1.8
+yum install -y java-1.8.0-openjdk wget
+
+# install server
+yum install -y puppetserver
+
+# configure puppetserver
+sed -i 's/JAVA_ARGS="-Xms2g -Xmx2g -Djruby.logger.class=com.puppetlabs.jruby_utils.jruby.Slf4jLogger"/JAVA_ARGS="-Xms1g -Xmx1g -Djruby.logger.class=com.puppetlabs.jruby_utils.jruby.Slf4jLogger"/g' /etc/sysconfig/puppetserver
+
+echo "[master]
+ dns_alt_names=master.puppet.io,puppet
+
+ [main]
+ certname = master.puppet.io
+ server = master.puppet.io
+ environment = production
+ runinterval = 10m" >> /etc/puppetlabs/puppet/puppet.conf
+
+# enable & start puppetserver
+systemctl enable puppetserver
