4b7047ceae4e37261202eb857edb8a46ea9cff97
Bohdan Pakhomov <shtormless@gmail.com>
2020-05-21 14:03:03 +0300

    nagios module deleted

--- nagios-module/nagios/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-# == Class: nagios
-class nagios {
-    include nagios::install
-    include nagios::fetch_file
-    include nagios::config
-    include nagios::services_master
-    include nagios::services_agent
-}
